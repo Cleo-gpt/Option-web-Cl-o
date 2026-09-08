0b8eb7f7372b722d157c5af30ec9e81091638d31
Cleo-gpt <pn34ydd@eduvaud.ch>
2026-09-08 14:30:58 +0200

    Add files via upload

---
+++ 03-donnees-nues/depart/js/meule.js
@@ -0,0 +1,20 @@
+const joursGruyere = 90;
+const poidsGruyere = 6;
+
+/* Étape 2 : écrivez ici le premier if, seulement pour le Gruyère. */
+
+class Meule {
+  constructor(nom, joursAffinage, poidsKg) {
+    this.nom = nom;
+    this.joursAffinage = joursAffinage;
+    this.poidsKg = poidsKg;
+  }
+
+  estPret() {
+    /* à vous : une ligne */
+  }
+}
+
+/* Étape 4 : fabriquez seulement le Gruyère ici. */
+
+/* Étape 5 : remplacez l’objet de test par la cave et son affichage. */
